20d263af5c8c5a54863a379a90c0378f25b5bd80
sh7411usa <48100468+sh7411usa@users.noreply.github.com>
2020-03-18 22:36:37 -0400

    Add files via upload

---
+++ VirtualFlip2/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(240, 400)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(12, 429)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(170, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(188, 427)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(43, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Send"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 413)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Send Text:"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(108, 413)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Mouse: 0, 0"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Timer2
+        '
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.Button1
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(240, 461)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "DEVICE"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ VirtualFlip2/Form1.vb
@@ -0,0 +1,204 @@
+﻿Public Class Form1
+
+    Dim imgC As Integer = 0
+    Dim x1 As Integer = 0, x2 As Integer = 0, y1 As Integer = 0, y2 As Integer = 0
+    Dim MouseSTR As String = "Mouse: ", MouseStillDown As Boolean = False
+    Dim LP As Integer = 0, LPb As Boolean = False
+    Dim tCn As Integer = 0, tcount As Boolean = False, tmax As String = 5
+
+    Sub ClearUp(temp As String)
+        Dim log As New Logging.Log("Form1.ClearUp")
+        If My.Computer.FileSystem.FileExists(temp) Then
+            Try
+                My.Computer.FileSystem.DeleteFile(temp)
+            Catch ex As Exception
+                log.WriteException(ex)
+            End Try
+        End If
+    End Sub
+
+    Function GetScreen(Optional UseAlt As Boolean = False) As Image
+        Dim Log As New Logging.Log("Form1.GetScreen"), lg As String = "Screencap Operation"
+        Dim temp As String = "temp.png"
+        If tcount Then
+            ClearUp(temp)
+            tCn += 1
+            temp = "temp" & tCn & ".png"
+            ClearUp(temp)
+            If tCn > tmax Then tCn = 0
+        End If
+
+        Dim localDir As String = temp, remDir As String = "/sdcard/download/" & temp
+
+        If Not UseAlt Then
+            lg += ", Regular method"
+            Log.WriteEntry(lg)
+            Log.WriteEntry(RunADBCommandNO("shell screencap -p " & remDir))
+            Log.WriteEntry(RunADBCommand("pull " & remDir & " " & localDir))
+        Else
+            lg += ", Using direct stream method"
+            Log.WriteEntry(lg)
+            Log.WriteEntry(RunADBCommandNO("exec-out screencap -p > " & localDir))
+        End If
+
+        Dim img As Bitmap = Nothing
+        Try
+            img = New Bitmap(Image.FromFile(localDir))
+        Catch ex As Exception
+            Log.WriteException(ex)
+        End Try
+
+        If IsNothing(img) And UseAlt = False Then
+            img = GetScreen(True)
+        End If
+
+        Return img
+    End Function
+
+
+
+    Sub UpdatePicBox()
+        Dim Log As New Logging.Log("Form1.UpdatePicBox")
+        If Not BackgroundWorker1.IsBusy Then
+            Log.WriteEntry("Calling BackgroundWorker1 to update screen")
+            BackgroundWorker1.RunWorkerAsync()
+        Else
+            Log.WriteEntry("BackgroundWorker1 is busy!")
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim Log As New Logging.Log("Form1.Timer1_Tick")
+        Log.WriteEntry("Tick! (Rate: " & Timer1.Interval & ")")
+        UpdatePicBox()
+    End Sub
+
+    Function RunADBCommand(args As String, Optional Serial As String = Nothing) As String
+        Dim Log As New Logging.Log("Form1.RunADBCommand")
+
+        Dim output As String, serror As String, regular As String
+        Dim prefix As String = ""
+        If Not IsNothing(Serial) Then
+            prefix = "-s " & Serial & " "
+        End If
+        Dim p As New Process
+        With p
+            .StartInfo.FileName = "adb.exe"
+            .StartInfo.Arguments = prefix & args
+            .StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+            .StartInfo.CreateNoWindow = True
+            .StartInfo.UseShellExecute = False
+            .StartInfo.RedirectStandardOutput = True
+            .StartInfo.RedirectStandardError = True
+        End With
+        Log.WriteEntry("Starting process: " & p.StartInfo.FileName & " " & p.StartInfo.Arguments)
+        p.Start()
+
+
+        regular = p.StandardOutput.ReadToEnd()
+        serror = p.StandardError.ReadToEnd()
+        Log.WriteEntry("Standard Output: " & regular.Trim())
+        Log.WriteEntry("Standard Error: " & serror.Trim())
+
+        If (serror.Trim() = "") Then
+            output = regular
+        Else
+            output = serror
+        End If
+
+        Return output
+    End Function
+    Function RunADBCommandNO(args As String, Optional Serial As String = Nothing, Optional wait As Boolean = False) As String
+        Dim log As New Logging.Log("Form1.RunADBCommandNO")
+        Dim prefix As String = ""
+        If Not IsNothing(Serial) Then
+            prefix = "-s " & Serial & " "
+        End If
+        log.WriteEntry("Shell Execute: adb.exe " & prefix & args)
+        Shell("adb " & prefix & args, AppWinStyle.Hide, wait, 5000)
+        Return "Reads No Output"
+    End Function
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        x2 = e.X : y2 = e.Y
+        Label2.Text = MouseSTR & e.X & ", " & e.Y
+    End Sub
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Try
+            PictureBox1.Image = GetScreen()
+        Catch ex As Exception
+            Dim log As New Logging.Log("BackgroundWorker1")
+            log.WriteException(ex)
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = RunADBCommand("get-serialno")
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Dim log As New Logging.Log("Timer2_Tick")
+        If MouseStillDown Then
+            LP += 1
+            log.WriteEntry("Longpress Counter: Tick! (" & LP & ")")
+            If Not (x1 = x2 And y1 = y2) Then
+                LP = 0
+                MouseSTR = "SWIPE: " & x1 & ", " & y1 & " "
+                Timer2.Enabled = False
+                Exit Sub
+            End If
+            If LP >= 20 Then
+                LPb = True
+                LP = 0
+                MouseSTR = "LONGPRESS: "
+                RunADBCommandNO("shell input longpress " & x1 & " " & y1)
+                Timer2.Enabled = False
+            End If
+        Else
+            LP = 0
+            MouseSTR = "Mouse: "
+            Timer2.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+        'set start x - y, start longpress timer
+        MouseSTR = "TAP: "
+        MouseStillDown = True
+        x1 = e.X : y1 = e.Y
+        Timer2.Enabled = True
+
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseUp
+        'send swipe or tap - as long as long press wasnt sent
+        MouseStillDown = False
+        MouseSTR = "Mouse: "
+        Dim log As New Logging.Log("Form1.PictureBox1_MouseUp")
+        x2 = e.X : y2 = e.Y
+        If Not LPb Then
+            If x1 = x2 And y1 = y2 Then
+                log.WriteEntry("Send Tap: " & x1 & " " & y1)
+                RunADBCommandNO("shell input tap " & x1 & " " & y1)
+            Else
+                log.WriteEntry("Send Swipe: " & x1 & " " & y1 & " " & x2 & " " & y2)
+                RunADBCommandNO("shell input swipe " & x1 & " " & y1 & " " & x2 & " " & y2)
+            End If
+        Else
+            LPb = False
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim log As New Logging.Log("Form1.Button1_Click")
+        log.WriteEntry("Sent Text: """ & TextBox1.Text & """")
+        RunADBCommandNO("shell input text '" & TextBox1.Text & "'")
+        TextBox1.Text = ""
+    End Sub
+End Class
