1e29be896b7876b4c3e0b2677967e2e075dcb502
sh7411usa <48100468+sh7411usa@users.noreply.github.com>
2020-03-22 08:09:49 -0400

    Add files via upload

---
+++ VirtualFlip2/Exalt_Keypad.vb
@@ -0,0 +1,210 @@
+﻿Imports System.Windows.Forms
+
+Public Class Exalt_Keypad
+
+    Dim MouseStillDown As Boolean = False, LP As Integer = 0, keyInt As Object = "0", LPSent As Boolean = False
+    Dim m As New SaveFileDialog
+
+    Sub SendKey(keynumber As String, Optional longPress As Boolean = False) '
+        Dim log As New Logging.Log("Exalt_Keypad.SendKey")
+        Dim pre As String = "shell input ", lpstring As String = ""
+        If longPress Then pre += "--longpress "
+        log.WriteEntry("Executing: ADB " & pre + "keyevent " + keynumber)
+        Dim otp As String = ""
+        If longPress Then
+            otp = "Long "
+        End If
+        otp += ("Keypress Sent: " + keynumber.ToString)
+        Form1.output(otp)
+        Form1.RunADBCommandNO(pre + "keyevent " + keynumber)
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Dim log As New Logging.Log("Exalt_Keypad.Timer2_Tick")
+        If MouseStillDown Then
+            LP += 1
+            log.WriteEntry("Longpress Counter: Tick! (" & LP & ")")
+            If LP >= 20 Then
+                LP = 0
+                LPSent = True
+                SendKey(GetKeyNum(keyInt), True)
+                Timer2.Enabled = False
+            End If
+        Else
+            LP = 0
+            Timer2.Enabled = False
+        End If
+    End Sub
+
+    Sub Buttons_MouseDown(sender As Object, e As MouseEventArgs) Handles _
+        Button1.MouseDown,
+        Button2.MouseDown,
+        Button3.MouseDown,
+        Button4.MouseDown,
+        Button5.MouseDown,
+        Button6.MouseDown,
+        Button7.MouseDown,
+        Button8.MouseDown,
+        Button9.MouseDown,
+        Button10.MouseDown,
+        Button11.MouseDown,
+        Button12.MouseDown,
+        Button13.MouseDown,
+        Button14.MouseDown,
+        Button15.MouseDown,
+        Button16.MouseDown,
+        Button17.MouseDown,
+        Button18.MouseDown,
+        Button19.MouseDown,
+        Button20.MouseDown,
+        Button21.MouseDown,
+        Button22.MouseDown,
+        Button23.MouseDown,
+        Button24.MouseDown,
+        Button25.MouseDown,
+        Button26.MouseDown,
+        Button27.MouseDown,
+        Button28.MouseDown,
+        Button30.MouseDown
+
+        keyInt = sender
+    End Sub
+
+    Sub Buttons_MouseUp(sender As Object, e As MouseEventArgs) Handles _
+        Button1.MouseUp,
+        Button2.MouseUp,
+        Button3.MouseUp,
+        Button4.MouseUp,
+        Button5.MouseUp,
+        Button6.MouseUp,
+        Button7.MouseUp,
+        Button8.MouseUp,
+        Button9.MouseUp,
+        Button10.MouseUp,
+        Button11.MouseUp,
+        Button12.MouseUp,
+        Button13.MouseUp,
+        Button14.MouseUp,
+        Button15.MouseUp,
+        Button16.MouseUp,
+        Button17.MouseUp,
+        Button18.MouseUp,
+        Button19.MouseUp,
+        Button20.MouseUp,
+        Button21.MouseUp,
+        Button22.MouseUp,
+        Button23.MouseUp,
+        Button24.MouseUp,
+        Button25.MouseUp,
+        Button26.MouseUp,
+        Button27.MouseUp,
+        Button28.MouseUp,
+        Button30.MouseUp
+
+        'run sendkey if not LPSent
+        If Not LPSent Then
+            SendKey(GetKeyNum(sender))
+        End If
+        LPSent = False
+    End Sub
+
+    Function GetKeyNum(sender As Object, Optional e As MouseEventArgs = Nothing) As String
+        Dim log As New Logging.Log("Exalt_Keypad.GetKeyNum")
+        Dim keyNumber As String = ""
+        log.WriteEntry("Called by: Name=""" & sender.name & """ Text=""" & sender.text & """")
+        Select Case sender.name
+            Case "Button1"
+                'Left Soft Key
+                keyNumber = "1"
+            Case "Button2"
+                'Right Soft Key
+                keyNumber = "2"
+            Case "Button3"
+                'Dpad Right
+                keyNumber = "22"
+            Case "Button4"
+                'Dpad Center
+                keyNumber = "23"
+            Case "Button5"
+                'Mic Key
+                keyNumber = "243"
+            Case "Button6"
+                'Dpad Left
+                keyNumber = "21"
+            Case "Button7"
+                'Dpad Up
+                keyNumber = "19"
+            Case "Button8"
+                'Speaker
+                keyNumber = "178"
+            Case "Button9"
+                'Dpad Down
+                keyNumber = "20"
+            Case "Button10"
+                'Clear
+                keyNumber = "28"
+            Case "Button11"
+                'TODO: Send
+                keyNumber = ""
+            Case "Button12"
+                'End
+                keyNumber = "6"
+            Case "Button13"
+                '3
+                keyNumber = "10"
+            Case "Button14"
+                '1
+                keyNumber = "8"
+            Case "Button15"
+                '2
+                keyNumber = "9"
+            Case "Button16"
+                '9
+                keyNumber = "16"
+            Case "Button17"
+                '7
+                keyNumber = "14"
+            Case "Button18"
+                '8
+                keyNumber = "15"
+            Case "Button19"
+                '6
+                keyNumber = "13"
+            Case "Button20"
+                '4
+                keyNumber = "11"
+            Case "Button21"
+                '5
+                keyNumber = "12"
+            Case "Button22"
+                '#
+                keyNumber = "18"
+            Case "Button23"
+                '*
+                keyNumber = "17"
+            Case "Button24"
+                '0
+                keyNumber = "7"
+            Case "Button25"
+                'Camera Key
+                keyNumber = "27"
+            Case "Button26"
+                'Vol+
+                keyNumber = "24"
+            Case "Button27"
+                'Vol-
+                keyNumber = "25"
+            Case "Button28"
+                'Home
+                keyNumber = "3"
+            Case "Button30"
+                'Power
+                keyNumber = "26"
+            Case Else
+                keyNumber = ""
+        End Select
+        log.WriteEntry("Key Selected: " & keyNumber)
+        Return keyNumber
+    End Function
+
+End Class
--- VirtualFlip2/Form1.Designer.vb
+++ VirtualFlip2/Form1.Designer.vb
@@ -31,12 +31,27 @@ Partial Class Form1
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FlipKeypadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InstallAnAppToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveScreenshotToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
+        Me.BackgroundWorker2 = New System.ComponentModel.BackgroundWorker()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.StatusL = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 24)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(240, 400)
         Me.PictureBox1.TabIndex = 0
@@ -46,17 +61,17 @@ Partial Class Form1
         '
         Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.TextBox1.Location = New System.Drawing.Point(12, 429)
+        Me.TextBox1.Location = New System.Drawing.Point(0, 465)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(170, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(199, 20)
         Me.TextBox1.TabIndex = 1
         '
         'Button1
         '
         Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Button1.Location = New System.Drawing.Point(188, 427)
+        Me.Button1.Location = New System.Drawing.Point(197, 464)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(43, 23)
+        Me.Button1.Size = New System.Drawing.Size(43, 22)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Send"
         Me.Button1.UseVisualStyleBackColor = True
@@ -65,7 +80,7 @@ Partial Class Form1
         '
         Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 413)
+        Me.Label1.Location = New System.Drawing.Point(3, 449)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(59, 13)
         Me.Label1.TabIndex = 3
@@ -75,7 +90,7 @@ Partial Class Form1
         '
         Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(108, 413)
+        Me.Label2.Location = New System.Drawing.Point(103, 449)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(63, 13)
         Me.Label2.TabIndex = 4
@@ -88,28 +103,142 @@ Partial Class Form1
         '
         'Timer2
         '
+        Me.Timer2.Enabled = True
         '
         'BackgroundWorker1
         '
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button2.Location = New System.Drawing.Point(92, 424)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(56, 22)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Home"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(18, 429)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(68, 13)
+        Me.LinkLabel1.TabIndex = 10
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Left Soft Key"
+        Me.LinkLabel1.Visible = False
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(153, 429)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(75, 13)
+        Me.LinkLabel2.TabIndex = 11
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Right Soft Key"
+        Me.LinkLabel2.Visible = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(240, 24)
+        Me.MenuStrip1.TabIndex = 12
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FlipKeypadToolStripMenuItem, Me.InstallAnAppToolStripMenuItem, Me.SaveScreenshotToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'FlipKeypadToolStripMenuItem
+        '
+        Me.FlipKeypadToolStripMenuItem.Name = "FlipKeypadToolStripMenuItem"
+        Me.FlipKeypadToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.FlipKeypadToolStripMenuItem.Text = "Flip Keypad"
+        '
+        'InstallAnAppToolStripMenuItem
+        '
+        Me.InstallAnAppToolStripMenuItem.Name = "InstallAnAppToolStripMenuItem"
+        Me.InstallAnAppToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.InstallAnAppToolStripMenuItem.Text = "Install an App"
+        '
+        'SaveScreenshotToolStripMenuItem
+        '
+        Me.SaveScreenshotToolStripMenuItem.Name = "SaveScreenshotToolStripMenuItem"
+        Me.SaveScreenshotToolStripMenuItem.Size = New System.Drawing.Size(159, 22)
+        Me.SaveScreenshotToolStripMenuItem.Text = "Save Screenshot"
+        '
+        'LinkLabel3
+        '
+        Me.LinkLabel3.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LinkLabel3.AutoSize = True
+        Me.LinkLabel3.Location = New System.Drawing.Point(68, 449)
+        Me.LinkLabel3.Name = "LinkLabel3"
+        Me.LinkLabel3.Size = New System.Drawing.Size(29, 13)
+        Me.LinkLabel3.TabIndex = 13
+        Me.LinkLabel3.TabStop = True
+        Me.LinkLabel3.Text = "PAD"
+        Me.LinkLabel3.Visible = False
+        '
+        'BackgroundWorker2
+        '
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StatusL})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 485)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(240, 22)
+        Me.StatusStrip1.TabIndex = 14
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'StatusL
+        '
+        Me.StatusL.Name = "StatusL"
+        Me.StatusL.Size = New System.Drawing.Size(39, 17)
+        Me.StatusL.Text = "Status"
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 1000
         '
         'Form1
         '
         Me.AcceptButton = Me.Button1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(240, 461)
+        Me.ClientSize = New System.Drawing.Size(240, 507)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.LinkLabel3)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "Form1"
         Me.ShowIcon = False
         Me.Text = "DEVICE"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -123,4 +252,17 @@ Partial Class Form1
     Friend WithEvents Timer1 As Timer
     Friend WithEvents Timer2 As Timer
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Button2 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents LinkLabel3 As LinkLabel
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FlipKeypadToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InstallAnAppToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveScreenshotToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BackgroundWorker2 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents StatusL As ToolStripStatusLabel
+    Friend WithEvents Timer3 As Timer
 End Class
